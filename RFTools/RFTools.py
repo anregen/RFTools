ced3abd2caac2a73321dbfc15175685e3e4f7663
Carl Denslow <carldenslow@zone5tech.com>
2018-12-21 15:29:22 -0800

    added distance calcs based on margin PEP8 fixes :p

--- RFTools/RFTools.py
+++ RFTools/RFTools.py
@@ -12,44 +12,52 @@ def fspl(freq_MHz=None, dist_km=None):
     else:
         return 32.44+(20*log10(freq_MHz))+(20*log10(dist_km))
 
+
 def cross_pol_gain(angle_mismatch_d):
     return pow(cos(radians(angle_mismatch_d)), 2)
+
 
 def ratio2dB(ratio):
     return 10*log10(ratio)
 
+
 def dB2ratio(dB):
     return pow(10, dB/10)
+
 
 def wavelength2freq(lam, vel_prop=1):
     return constants.c*vel_prop/lam
 
+
 def freq2wavelength(f, vel_prop=1):
     return constants.c*vel_prop/f
+
 
 class LinkCalc:
     freq_MHz = None
     dist_km = None
-    ant_gain = None
+    tx_ant_gain = None
+    rx_ant_gain = None
     radio_sensitivity = None
     radio_tx_power = None
 
-    def __init__(self, freq_MHz, dist_km, ant_gain, radio_tx_power, radio_sensitivity):
+    def __init__(self, freq_MHz, dist_km, tx_ant_gain, rx_ant_gain, radio_tx_power, radio_sensitivity):
         self.freq_MHz = freq_MHz
         self.dist_km = dist_km
-        self.ant_gain = ant_gain
+        self.tx_ant_gain = tx_ant_gain
+        self.rx_ant_gain = rx_ant_gain
         self.radio_tx_power = radio_tx_power
         self.radio_sensitivity = radio_sensitivity
 
     def margin(self, dist_km=None):
         if dist_km is None:
             dist_km = self.dist_km
-        eirp = self.radio_tx_power+self.ant_gain
+        eirp = self.radio_tx_power+self.tx_ant_gain
         pg = -1*fspl(self.freq_MHz, dist_km)
-        return (eirp+pg)-self.radio_sensitivity
+        return eirp + pg + self.rx_ant_gain - self.radio_sensitivity
 
     def distance(self, margin_dB=3):
-        acceptable_pl = self.radio_tx_power + self.ant_gain + self.ant_gain - self.radio_sensitivity - margin_dB
+        acceptable_pl = self.radio_tx_power + self.tx_ant_gain + self.rx_ant_gain - self.radio_sensitivity - margin_dB
         dist_m = 10 ** ((acceptable_pl + 20 * log10(300000000 / (4 * pi * self.freq_MHz * 1000000))) / 20)
         return dist_m/1000.0
 
